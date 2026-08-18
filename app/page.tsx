88f336b539898fbf0d1eefdc2dad55582109b505
wpseemol <wpseemol@gmail.com>
2026-08-18 17:15:39 +0600

    bran section is done

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import BrandsMarquee from "./_components/BrandsMarquee";
 import HeroSection from "./_components/hero/HeroSection";
+import StatsSection from "./_components/StatsSection";
 
 export default function Home() {
     return (
         <main className="">
             <HeroSection />
+            <StatsSection />
+            <BrandsMarquee />
         </main>
     );
 }
