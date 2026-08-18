053a470111b874d34bf5e7c043256551596b3763
wpseemol <wpseemol@gmail.com>
2026-08-18 18:16:49 +0600

    precess section will done now

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import BrandsMarquee from "./_components/BrandsMarquee";
 import HeroSection from "./_components/hero/HeroSection";
+import ProcessSection from "./_components/ProcessSection";
+import ServicesMarquee from "./_components/ServicesMarquee";
 import StatsSection from "./_components/StatsSection";
 
 export default function Home() {
@@ -8,6 +10,8 @@ export default function Home() {
             <HeroSection />
             <StatsSection />
             <BrandsMarquee />
+            <ServicesMarquee />
+            <ProcessSection />
         </main>
     );
 }
