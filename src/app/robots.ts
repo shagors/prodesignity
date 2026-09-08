e875483fd06050fd7a4803c81ad811f38a166554
wpseemol <wpseemol@gmail.com>
2026-09-08 12:40:57 +0600

    solve build erro

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,6 +1,8 @@
 // src/app/robots.ts
-import { MetadataRoute } from "next";
+import type { MetadataRoute } from "next";
 import { siteConfig } from "@/config/site";
+
+export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
     const baseUrl = siteConfig?.url || "https://prodesignity.com";
